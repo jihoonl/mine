7238940f071750d596b858fa3f4146d6cca1b470
Jihoon Lee <jihoonlee.in@gmail.com>
2017-06-03 20:43:05 +0000

    Use configuration for cheerup messages

--- scripts/server.py
+++ scripts/server.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
+import os
 from geungjungbot import GeungjungServer as Server
 
 
 if __name__ == '__main__':
-    Server.spin(host='localhost', port=5353, debug=True)
+
+    config_file = os.environ['GEUNGJUNG_CONFIG']
+
+    Server.spin(config_file, host='localhost', port=5353, debug=True)
--- src/geungjungbot/server.py
+++ src/geungjungbot/server.py
@@ -1,5 +1,8 @@
 # -*- coding:utf-8 -*-
 
+from random import randint
+import traceback
+import yaml
 import logging as log
 import sys
 from flask import request, jsonify, Flask,  make_response
@@ -7,9 +10,15 @@ from flask_restful import Api
 
 log.basicConfig(stream=sys.stdout, level=log.DEBUG)
 
-
 app = Flask(__name__)
 api = Api(app)
+
+MODE = {
+    'repeat': True
+}
+
+config = None
+
 
 @api.representation('application/json')
 @app.route('/keyboard', methods=['GET'])
@@ -21,19 +30,46 @@ def received_keyboard():
     }
     return make_response(jsonify(message), 200)
 
+
 @api.representation('application/json')
 @app.route('/message', methods=['POST'])
 def received_message():
     log.info("Received Message")
     log.info(request.json)
-    d = request.json
-    log.info(d['content'].encode('utf-8'))
-    message = {
+
+    content = request.json['content'].encode('utf-8')
+
+    try:
+        if content == '칭찬해줘' or content == '칭찬':
+            idx = randint(0, len(config) - 1)
+            message = config[idx]
+        elif content == '나를 따라해':
+            if not MODE['repeat']:
+                MODE['repeat'] = True
+                message = '응 따라할게'
+        elif content == '나를 그만 따라해':
+            if MODE['repeat']:
+                MODE['repeat'] = False
+                message = '응 그만 따라할게'
+        elif content == '모드':
+            message = '나를 따라해, 나를 그만 따라해'
+        else:
+            if MODE['repeat']:
+                message = content
+            else:
+                message = '잠깐만...'
+    except Exception as e:
+        log.error(e)
+        traceback.print_exc(file=sys.stdout)
+        message = '에러났어.. %s' % e
+
+    response = {
         "message": {
-            "text": d['content'].encode('utf-8')
+            "text": message
         }
     }
-    return make_response(jsonify(message), 200)
+    return make_response(jsonify(response), 200)
+
 
 @api.representation('application/json')
 @app.route('/friend', methods=['POST'])
@@ -43,6 +79,7 @@ def received_friend():
     message = {}
     return make_response(jsonify(message), 200)
 
+
 @api.representation('application/json')
 @app.route('/friend/<user_key>', methods=['DELETE'])
 def received_friend_name(user_key):
@@ -50,6 +87,7 @@ def received_friend_name(user_key):
     log.info(request.json)
     message = {}
     return make_response(jsonify(message), 200)
+
 
 @api.representation('application/json')
 @app.route('/chat_room/<user_key>', methods=['DELETE'])
@@ -59,5 +97,15 @@ def received_chatroom_delete(user_key):
     message = {}
     return make_response(jsonify(message), 200)
 
-def spin(host='localhost', port=5000, debug=True):
+
+def _load_config(config_file):
+    with open(config_file) as f:
+        c = yaml.load(f)
+    log.info(c)
+    return c['cheerup']
+
+
+def spin(config_file, host='localhost', port=5000, debug=True):
+    global config
+    config = _load_config(config_file)
     app.run(host=host, port=port, debug=debug)
